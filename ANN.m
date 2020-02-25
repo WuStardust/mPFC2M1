695fc878dcb8b7735a2b32dd44cbc2acb8620057
ShengHui_Wu <770577856@qq.com>
2020-02-25 22:56:22 +0800

    追加随机次数

--- ANN.m
+++ ANN.m
@@ -5,6 +5,7 @@ rng('default')
 addpath models/
 addpath utils/
 load("data/data_rat010_0615_spike_train_selected.mat")
+load("results/ANN_explore_Nz.mat", "ANN_explore_Nz")
 %% Grid search
 % explore Nz as H=5, rng use default
 % 
@@ -12,21 +13,22 @@ load("data/data_rat010_0615_spike_train_selected.mat")
 % 
 % result seems not good, pretrain needed?
 
-ANN_explore_Nz = struct( ...
-  "H",{}, "xi1",{}, "xi2",{}, "mu",{}, "thres",{}, "iterThres",{}, ...
-  "maxIter",{}, "alpha",{}, "M1Idx",{}, "s",{}, ...
-  "W",{}, "L",{}, "DBR",{}, "Lval",{}, "LHistory",{} ...
-  );
+% ANN_explore_Nz = struct( ...
+%   "H",{}, "xi1",{}, "xi2",{}, "mu",{}, "thres",{}, "iterThres",{}, ...
+%   "maxIter",{}, "alpha",{}, "M1Idx",{}, "s",{}, ...
+%   "W",{}, "L",{}, "DBR",{}, "Lval",{}, "LHistory",{} ...
+%   );
+rng(ANN_explore_Nz(400).s)
 NzSearchNum = 20;
-repeatNum = 20;
+repeatNum = 60;
 M1Idx = 1; % select M1 neuron
 M1spikePart = M1spike(:,M1Idx);
 tic
 parfor i=1:NzSearchNum*repeatNum
-  Nzlist = 1:20;
+  Nzlist = 1:NzSearchNum;
   s=rng;
-  Nz = Nzlist(ceil(i/20));
-  H = 5; % temporal history, todo: grid search
+  Nz = Nzlist(ceil(i/repeatNum));
+  H = 7; % temporal history, todo: grid search
   xi1 = 0.05; % first stage weight parameters initial range param
   xi2 = 0.1; % second stage weight parameters initial range param
   mu = 1000; % modified LM algorithm param
@@ -43,14 +45,14 @@ parfor i=1:NzSearchNum*repeatNum
     "W",W, "L",L, "DBR",DBR, "Lval",Lval, "LHistory",LHistory ...
     );
   if (i/10 == 0)
-    disp(['      Nz ', num2str(Nz, '%02d'), ' idx ', num2str(mod(i-1, 20)+1, '%02d'), ' Lval ', num2str(Lval)])
+    disp(['      Nz ', num2str(Nz, '%02d'), ' idx ', num2str(mod(i-1, repeatNum)+1, '%02d'), ' Lval ', num2str(Lval)])
   end
 end
 toc
-Nzlist = 1:20;
+Nzlist = 1:NzSearchNum;
 for i=1:NzSearchNum*repeatNum
-  Nz = Nzlist(ceil(i/20));
-  idx = mod(i-1, 20)+1;
+  Nz = Nzlist(ceil(i/repeatNum));
+  idx = mod(i-1, repeatNum)+1+20;
   ANN_explore_Nz(Nz, idx) = results{i};
 end
-save("results/ANN_explore_Nz.mat", "ANN_explore_Nz")
+save("results/ANN_explore_Nz_more.mat", "ANN_explore_Nz")
